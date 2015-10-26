5db77ff4778acff1bdb8472f944e44fd8add90f8
kikill95 <nokia6030>
2015-10-26 21:33:52 +0200

    Adding first logic to the project

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,15 @@
 class UsersController < ApplicationController
-  def new
+  def create
+    @user = User.new(params.require(:user).permit!)
+    if @user.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
-  def create
+  def new
+    @user = User.new
   end
 
   def destroy
